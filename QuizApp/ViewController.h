69d2bcbff49752330a9a36de13d8e2362db8a043
Haruka <Haruka@haruchan-mac.local>
2016-02-01 20:07:52 +0900

    test

--- QuizApp/ViewController.h
+++ QuizApp/ViewController.h
@@ -9,6 +9,18 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+-(IBAction)nextQuestion:(id)sender;   //次の問題へ行く
+-(IBAction)backQuestion:(id)sender;   //次の問題へ行く
+-(IBAction)chooseAnswer:(id)sender; //回答を選択する
+-(IBAction)judgeGrade:(id)sender;   //正答率を元に評価を表示する
+-(IBAction)calData:(id)sender;  //正答率を計算する
+@property(nonatomic,weak) IBOutlet UITextView *question;
+@property(nonatomic,weak) IBOutlet UILabel *grade;
+@property(nonatomic,weak) IBOutlet UILabel *percentage;
+@property(nonatomic,weak) IBOutlet UIButton *correct;
+@property(nonatomic,weak) IBOutlet UIButton *incorrect;
+@property(nonatomic,weak) IBOutlet UIButton *next;
+@property(nonatomic,weak) IBOutlet UIButton *back;
 
 
 @end
--- QuizApp/ViewController.m
+++ QuizApp/ViewController.m
@@ -8,15 +8,139 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
+    UITextView *question;
+    UILabel *grade;
+    UILabel *percentage;
+    UIButton *back;
+    UIButton *next;
+    UIButton *correct;
+    UIButton *incorrect;
+    int num;
+    int cnt;
+}
 
 @end
 
 @implementation ViewController
 
+- (void)setupParts{
+    self.question.text =@"問題文が入ります。";
+    question.textAlignment = NSTextAlignmentLeft;
+
+    question.backgroundColor = [UIColor clearColor];
+    [self.view addSubview:question];
+
+    self.grade.text =@"評価が入ります。";
+    grade.textAlignment = NSTextAlignmentLeft;
+    grade.backgroundColor = [UIColor clearColor];
+    [self.view addSubview:grade];
+
+    percentage.text =@"正答率が入ります。";
+    percentage.textAlignment = NSTextAlignmentLeft;
+    percentage.backgroundColor = [UIColor clearColor];
+    [self.view addSubview:percentage];
+
+    correct = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [self.view addSubview:correct];
+    
+    incorrect = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [self.view addSubview:incorrect];
+    
+    next = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [next addTarget:self action:@selector(nextQuestion:) forControlEvents:UIControlEventTouchUpInside];
+    num = 1;
+/*
+    back = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [back addTarget:self action:@selector(backQuestion:) forControlEvents:UIControlEventTouchUpInside];
+*/
+    cnt = 1;
+}
+
+
+- (IBAction)nextQuestion:(id)sender{
+    NSLog(@"nextQuestionを実行します");
+    //次の問題へ遷移する
+    switch (num) {
+        case 1:
+            self.question.text = @"question2";
+            [self.view addSubview:question];
+            cnt = 1;
+            num = 2;
+            break;
+        case 2:
+            self.question.text = @"question3";
+            [self.view addSubview:question];
+            cnt = 2;
+            num = 3;
+            break;
+        case 3:
+            self.question.text = @"question4";
+            cnt = 3;
+            num = 4;
+            break;
+        case 4:
+            self.question.text = @"question5";
+            cnt = 4;
+            break;
+        default:
+            
+            break;
+    }
+
+    back = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [back addTarget:self action:@selector(backQuestion:) forControlEvents:UIControlEventTouchUpInside];
+    cnt = 1;
+    [self.view addSubview:back];
+
+
+}
+
+
+- (IBAction)backQuestion:(id)sender{
+    NSLog(@"backQuestionを実行します");
+    switch (cnt) {
+        case 1:
+            self.question.text = @"問題1";
+            num = 2;
+            [self.view addSubview:question];
+            break;
+        case 2:
+            self.question.text = @"問題2";
+            [self.view addSubview:question];
+            break;
+        case 3:
+            self.question.text = @"問題2";
+            [self.view addSubview:question];
+            break;
+        case 4:
+            self.question.text = @"問題2";
+            [self.view addSubview:question];
+            break;
+        default:
+            break;
+    }
+}
+
+- (void)chooseAnswer:(id)sender{
+    //回答を選択する
+
+}
+
+- (void)judgeGrade:(id)sender{
+    //正答率を元に評価を表示する
+}
+
+- (void)calData:(id)sender{
+    //正答率を計算する
+
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self setupParts];
+    num = 1;
 }
 
 - (void)didReceiveMemoryWarning {
